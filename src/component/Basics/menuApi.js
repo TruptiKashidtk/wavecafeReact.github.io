38b98ce420a47bc86b623dafa542b7c874df5981
TruptiKashidtk <127821847+TruptiKashidtk@users.noreply.github.com>
2023-03-14 13:12:31 +0530

    Wave cafe
    using react material

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,155 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/iced-espresso.png",
+    name: "Espresso",
+    category: "Iced Coffe",
+    price: "12₹",
+    description:
+      "The iced espresso is a classic drink that you can enjoy on any hot day. Its made by pouring espresso shots into an ice-filled glass, then adding cold tap water to dilute it just enough for your liking!. ",
+  },
+
+  {
+    id: 2,
+    image: "images/hot-americano.png",
+    name: "Americano",
+    category: "Hot Coffee",
+    price: "20₹",
+    description:
+      "Americano coffee is served in nearly every cafe in the world. If your favorite coffee shop has an espresso machine, they can make you a deliciously bold Americano.. ",
+  },
+
+  {
+    id: 3,
+    image: "../images/smoothie-1.png",
+    name: "Strawberry smoothies",
+    category: "Fruit Juice",
+    price: "80₹",
+    description:
+      "Start your day off right with these creamy strawberry smoothies. Vegan. Gluten-free, dairy-free, and sugar-free options too. Pin Share Tweet Email Save. Fruit and Yogurt Smoothie.. ",
+  },
+  {
+    id: 4,
+    image: "images/special-03.jpg",
+    name: "Burger",
+    category: "Special",
+    price: "12₹",
+    description:
+      " Drain the veggie patties on paper towels to remove excess oil. 8. Toasting burger buns. 34. Slice the burger buns in equal halves. Spread some butter on the burger bun slices.. ",
+  },
+
+  {
+    id: 5,
+    image: "images/iced-cappuccino.png",
+    name: "Cold Cappuccino",
+    category: "Iced Coffe",
+    price: "10₹",
+    description:
+      "A cappuccino is an espresso-based coffee drink that originated in Austria and was later popularized in Italy and is prepared with steamed milk foam (microfoam). ",
+  },
+  {
+    id: 6,
+    image: "../images/hot-cappuccino.png",
+    name: "Hot Cappuccino",
+    category: "Hot Coffee",
+    price: "50₹",
+    description:
+      "A cappuccino is an espresso-based coffee drink that originated in Austria and was later popularized in Italy and is prepared with steamed milk foam (microfoam).. ",
+  },
+  {
+    id: 7,
+    image: "../images/hot-espresso.png",
+    name: "Hot Espresso",
+    category: "Hot Coffee",
+    price: "180₹",
+    description:
+      "Espresso is made by forcing very hot water under high pressure through finely ground compacted coffee. There is no universal standard defining the process of extracting espresso . ",
+  },
+  {
+    id: 8,
+    image: "../images/hot-latte.png",
+    name: "Caffè latte",
+    category: "Hot Coffee",
+    price: "60₹",
+    description:
+      "Caffè latte , often shortened to just latte  in English, is a coffee beverage of Italian origin made with espresso and steamed milk. Variants include the chocolate-flavored . ",
+  },
+  {
+    id: 9,
+    image: "images/special-04.jpg",
+    name: "Cake",
+    category: "Special",
+    price: "12₹",
+    description:
+      "Cake is a flour confection made from flour, sugar, and other ingredients, and is usually baked. In their oldest forms, cakes were modifications of bread, but cakes now cover a wide range of preparations that can be simple or elaborate. ",
+  },
+
+
+  {
+    id: 10,
+    image: "../images/smoothie-2.png",
+    name: "cherry smoothie",
+    category: "Fruit Juice",
+    price: "60₹",
+    description:
+      "This cherry smoothie is made with juice, yogurt, frozen dark cherries and banana, all blended together into a creamy and delicious drink. A kid friendly smoothie that perfectly sweet and only takes 5 minutes to make.. ",
+  },
+  {
+    id: 11,
+    image: "../images/iced-latte.png",
+    name: "Ice Latte",
+    category: "Iced Coffe",
+    price: "10₹",
+    description:
+      "Caffè latte , often shortened to just latte  in English, is a coffee beverage of Italian origin made with espresso and steamed milk. Variants include the chocolate-flavored . ",
+  },
+  {
+    id: 12,
+    image: "images/iced-americano.png",
+    name: "Iced Americano",
+    category: "Iced Coffe",
+    price: "12₹",
+    description:
+      "Americano coffee is served in nearly every cafe in the world. If your favorite coffee shop has an espresso machine, they can make you a deliciously bold Americano.. ",
+  },
+  {
+    id: 13,
+    image: "images/special-06.jpg",
+    name: "Sandwich",
+    category: "Special",
+    price: "12₹",
+    description:
+      "A sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread, or more generally any dish wherein bread serves as a container or wrapper for another food type. . ",
+  },
+
+  {
+    id: 14,
+    image: "images/smoothie-3.png",
+    name: "Papaya",
+    category: "Fruit Juice",
+    price: "12₹",
+    description:
+      "Papaya is packed with antioxidant nutrients such as carotenes and vitamin C.These nutrients may be helpful to promote against heart disease including atherosclerosis and diabetic heart disease. . ",
+  },
+  {
+    id: 15,
+    image: "images/smoothie-4.png",
+    name: "Vegetable smoothie",
+    category: "Fruit Juice",
+    price: "12₹",
+    description:
+      "Vegetable smoothie contains 190 calories per 216 g serving. This serving contains 14 g of fat, 6 g of protein and 13 g of carbohydrate. The latter is 5.6 g sugar and 6.3 g of dietary fiber, the rest is complex carbohydrate. . ",
+  },
+  {
+    id: 16,
+    image: "images/special-02.jpg",
+    name: "Bread Roll",
+    category: "Special",
+    price: "12₹",
+    description:
+      "These are crispy and tasty Bread Roll with a savory, tangy spiced mashed potato filling. Bread roll is a tasty Indian snack that can be made in a jiffy if you have leftover boiled potatoes. ",
+  },
+
+];
+
+export default Menu;
